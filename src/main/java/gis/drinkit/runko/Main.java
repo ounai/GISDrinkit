03ee443333f4259dab6c64fde8a58a96b2b4014f
ounai <git@ounai.fi>
2018-02-12 13:55:45 +0200

    Web-sovelluksen aloitus + siivousta

--- src/main/java/gis/drinkit/runko/Main.java
+++ src/main/java/gis/drinkit/runko/Main.java
@@ -3,6 +3,10 @@ package gis.drinkit.runko;
 import java.io.File;
 import spark.Spark;
 import gis.drinkit.runko.database.Database;
+import gis.drinkit.runko.database.DrinkkiDao;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Main {
 
@@ -12,34 +16,29 @@ public class Main {
         Database tietokanta = new Database("jdbc:sqlite:" + tietokantaTiedosto.getAbsolutePath());
         tietokanta.init();
         
-        Spark.get("/", (req, res) -> {
-            return "testi";
-        });
+        DrinkkiDao drinkkiDao = new DrinkkiDao(tietokanta);
         
-        /*Database database = new Database("jdbc:sqlite:opiskelijat.db");
-        database.init();
-
-        OpiskelijaDao opiskelijaDao = new OpiskelijaDao(database);
-
-        get("/", (req, res) -> {
-            HashMap map = new HashMap<>();
-            map.put("viesti", "tervehdys");
-
+        Spark.get("/", (req, res) -> {
+            HashMap map = new HashMap();
+            
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
-
-        get("/opiskelijat", (req, res) -> {
-            HashMap map = new HashMap<>();
-            map.put("opiskelijat", opiskelijaDao.findAll());
-
-            return new ModelAndView(map, "opiskelijat");
+        
+        Spark.get("/drinkit", (req, res) -> {
+            HashMap map = new HashMap();
+            map.put("drinkit", drinkkiDao.findAll());
+            
+            return new ModelAndView(map, "drinkit");
         }, new ThymeleafTemplateEngine());
-
-        get("/opiskelijat/:id", (req, res) -> {
-            HashMap map = new HashMap<>();
-            map.put("opiskelija", opiskelijaDao.findOne(Integer.parseInt(req.params("id"))));
-
-            return new ModelAndView(map, "opiskelija");
-        }, new ThymeleafTemplateEngine());*/
+        
+        Spark.get("/drinkit/:id", (req, res) -> {
+            HashMap map = new HashMap();
+            Integer id = Integer.parseInt(req.params("id"));
+            
+            map.put("drink", drinkkiDao.findOne(id));
+            
+            return new ModelAndView(map, "drinkki");
+        }, new ThymeleafTemplateEngine());
+        
     }
 }
